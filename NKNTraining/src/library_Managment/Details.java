c5c51fa35346f557d9703b09a9fbb010d5004663
Nikamnarendra <nikamnarendra.02@gmail.com>
2025-05-07 19:00:03 +0530

    Library Management program added

---
+++ NKNTraining/src/library_Managment/Details.java
@@ -0,0 +1,56 @@
+package library_Managment;
+
+public class Details {
+
+	public class BookDetails
+	{
+		String Titleofthebook = "Software Testing Principles and Practice";
+
+		String Authorname = "Shrinivasan Desikan";
+
+		int Yearofpublication = 2006;
+
+		float Priceofthebook = 500.10F;
+
+		String bookavailablity = "Yes";
+	
+		
+	}
+	public static void main(String[] args) 
+	{
+		//Book Details
+		String Titleofthebook = "Software Testing Principles and Practice";
+		String Authorname = "Shrinivasan Desikan";
+		int Yearofpublication = 2006;
+		float Priceofthebook = 500.10F;
+		boolean bookavailablity = true;
+		//Member Details
+		String MemberName = "Narendra Nikam";
+		int MemberId = 1001;
+		char MembershipType = 'A'; //A=Adult, C=Child, S=Senior
+		//Borrowing Status
+		int NoOfDaysBookBorrowed = 5;
+		float FinePerDay = 10.00F;
+		float TotalFine = (NoOfDaysBookBorrowed*FinePerDay);
+		
+	System.out.println("Library Managemnet System");
+	System.out.println("");
+	System.out.println("Book Details:");
+	System.out.println("Title of The Book   : "+ Titleofthebook);
+	System.out.println("Author's Name       : "+ Authorname);
+	System.out.println("Year of Publication : "+ Yearofpublication);
+	System.out.println("Price of the Book   : "+ Priceofthebook);
+	System.out.println("Book is available   : "+ bookavailablity);
+	System.out.println("");
+	System.out.println("Member Details:");
+	System.out.println("Name of the member   : "+ MemberName);
+	System.out.println("Member ID number     : "+ MemberId);
+	System.out.println("Type of membership   : "+ MembershipType);
+	System.out.println("");
+	System.out.println("Borrowing Status:");
+	System.out.println("Number of days book borrowed   : "+ NoOfDaysBookBorrowed);
+	System.out.println("Fine per extra day             : Rupees "+ FinePerDay);
+	System.out.println("Total fine to be Paid          : Rupees "+ TotalFine);
+	}
+
+}
