50a07bef8673e61ae92ef0d2fb22ee90e8028355
Nikamnarendra <nikamnarendra.02@gmail.com>
2025-05-08 14:51:22 +0530

    Assignmnent program updated with comments

--- NKNTraining/src/operators/OperatorAssignmentBilling.java
+++ NKNTraining/src/operators/OperatorAssignmentBilling.java
@@ -4,20 +4,20 @@ public class OperatorAssignmentBilling {
 
 	public static void main(String[] args) {
 		
-		int noofItemsPurchased = 5;
-		int priceofeachitem = 1000;
-		int totalbillbeforediscount = (noofItemsPurchased*priceofeachitem);
-		boolean eligiblefordiscount = (totalbillbeforediscount>1000);
+		int noofItemsPurchased = 5; // Unary operator
+		int priceofeachitem = 1000; // Unary operator
+		int totalbillbeforediscount = (noofItemsPurchased*priceofeachitem); // Unary operator
+		boolean eligiblefordiscount = (totalbillbeforediscount>1000);// Unary operator 
 		int applydiscount =(totalbillbeforediscount- ((totalbillbeforediscount)/100)*10);
-		int totalbillafterdiscount = (totalbillbeforediscount>1000) ? applydiscount : totalbillbeforediscount;
+		int totalbillafterdiscount = (totalbillbeforediscount>1000) ? applydiscount : totalbillbeforediscount; // Ternary operators
 		System.out.println("Total No. of Items Purchased : "+noofItemsPurchased);
 		System.out.println("Eligible for Discount        : "+eligiblefordiscount);
 		System.out.println("Total Bill Before Discount   : "+totalbillbeforediscount+"/-");
 		System.out.println("Total Bill After Discount    : "+totalbillafterdiscount+"/-");
 		
-		int itemadded= ++noofItemsPurchased;
+		int itemadded= ++noofItemsPurchased; // Unary operator : increment operator
 		
-		boolean eligibleforgift = (itemadded>5) && (totalbillafterdiscount>1000) ;
+		boolean eligibleforgift = (itemadded>5) && (totalbillafterdiscount>1000) ; // Logical operators
 		//System.out.println("Free Gift    : "+itemadded);
 		System.out.println("Free Gift    : "+eligibleforgift);
 		
